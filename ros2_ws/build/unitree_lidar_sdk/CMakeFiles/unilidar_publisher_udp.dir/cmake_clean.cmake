15b4f04cca4cc335a3e1a5031423b92cbd220b07
Pascual Asio Serrano <103388150+pokepascu@users.noreply.github.com>
2026-07-29 14:32:30 +0100

    Add files via upload

---
+++ ros2_ws/build/unitree_lidar_sdk/CMakeFiles/unilidar_publisher_udp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/workspace/ros2_ws/src/unilidar_sdk/unitree_lidar_sdk/bin/unilidar_publisher_udp"
+  "/workspace/ros2_ws/src/unilidar_sdk/unitree_lidar_sdk/bin/unilidar_publisher_udp.pdb"
+  "CMakeFiles/unilidar_publisher_udp.dir/examples/unilidar_publisher_udp.cpp.o"
+  "CMakeFiles/unilidar_publisher_udp.dir/examples/unilidar_publisher_udp.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unilidar_publisher_udp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros2_ws/build/unitree_lidar_sdk/CMakeFiles/unilidar_subscriber_udp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for unilidar_subscriber_udp.
+# This may be replaced when dependencies are built.
